afd4e51c8f2a38eb6a40522186558916517ad211
Kaan <kaanakdenz@gmail.com>
2020-02-05 01:08:49 +0300

    Simple Quiz Application

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import './quiz.dart';
+import './result.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ]
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 10},
+        {'text': 'Lion', 'score': 5},
+        {'text': 'Elephant', 'score': 3},
+        {'text': 'Snake', 'score': 1},
+      ]
+    },
+    {
+      'questionText': 'What\'s your favorite sport?',
+      'answers': [
+        {'text': 'Football', 'score': 10},
+        {'text': 'Basketball', 'score': 5},
+        {'text': 'Golf', 'score': 3},
+        {'text': 'Race', 'score': 1},
+      ]
+    },
+  ];
+
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+    setState(() {
+      _questionIndex += 1;
+    });
+  }
+
+  void _resetQuiz() {
+    setState(() {});
+    _questionIndex = 0;
+    _totalScore = 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
+      ),
+    );
+  }
+}
